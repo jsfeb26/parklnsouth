a437e7682fdc7a2f7e46cffd86a80f03ce54b344
Jason Stinson <jstinson@atlassian.com>
2018-04-30 01:30:12 -0400

    update names of site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Dimension by HTML5 UP"
+    title: "ParkLnSouth Website",
+    author: "Jason Stinson",
+    description: "ParkLnSouth Website"
   },
   pathPrefix: '/',
   plugins: [
